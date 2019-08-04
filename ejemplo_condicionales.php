b0e0a172d48de9b344d2a0ecf8bd6e519014f8ab
erisjimver <erisjinver@gmail.com>
2019-08-04 17:45:24 -0500

    agregando archivos condicionales switch

---
+++ ejemplo_condicionales.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+<h1>USANDO CONDICIONALES</h1>
+
+<form action="validacion_condicionales.php" method="post" name="datos_usuario" id="datos_usuario">
+  <table width="15%" align="center">
+    <tr>
+      <td>Nombre:</td>
+      <td><label for="nombre_usuario"></label>
+      <input type="text" name="nombre_usuario" id="nombre_usuario"></td>
+    </tr>
+    <tr>
+      <td>Contraseña:</td>
+      <td><label for="edad_usuario"></label>
+      <input type="text" name="contra" id="contra"></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="2" align="center"><input type="submit" name="enviando" id="enviando" value="Enviar"></td>
+    </tr>
+  </table>
+</form>
+
+
+
+</body>
+</html>
